25ccb0f64892619051ce515c69d31bbc61458493
齐选龙 <qixuanlong@qixuanlongdeMacBook-Pro.local>
2021-07-15 19:45:57 +0800

    2021/7/15 19:45

--- initial.sql
+++ initial.sql
@@ -1,11 +1,11 @@
 USE fastapi;
 
 -- Username,  Password, Identity
-INSERT INTO Information VALUES ('admin1', '123', 'Manager');
-INSERT INTO Information VALUES ('admin2', '123', 'Manager');
-INSERT INTO Information VALUES ('admin3', '123', 'Manager');
-INSERT INTO Information VALUES ('admin4', '123', 'Manager');
-INSERT INTO Information VALUES ('admin5', '123', 'Manager');
+INSERT INTO information VALUES ('admin1', '123', 'Manager');
+INSERT INTO information VALUES ('admin2', '123', 'Manager');
+INSERT INTO information VALUES ('admin3', '123', 'Manager');
+INSERT INTO information VALUES ('admin4', '123', 'Manager');
+INSERT INTO information VALUES ('admin5', '123', 'Manager');
 -- INSERT INTO Information VALUES ('', '', '');
 -- INSERT INTO Information VALUES ('', '', '');
 -- INSERT INTO Information VALUES ('', '', '');
@@ -16,11 +16,11 @@ INSERT INTO Information VALUES ('admin5', '123', 'Manager');
 
 
 -- Belong, Name, Manager_id
-INSERT INTO Shop (Shop_id, Belong, Name, Manager_id) VALUES (1, '美食园', '煎饼', 'admin1');
-INSERT INTO Shop (Shop_id, Belong, Name, Manager_id) VALUES (2, '美食园', '麻辣香锅', 'admin2');
-INSERT INTO Shop (Shop_id, Belong, Name, Manager_id) VALUES (3, '美食园', '湘里香外', 'admin3');
-INSERT INTO Shop (Shop_id, Belong, Name, Manager_id) VALUES (4, '美食园', '强子烤冷面', 'admin4');
-INSERT INTO Shop (Shop_id, Belong, Name, Manager_id) VALUES (5, '美食园', '沙县小吃', 'admin5');
+INSERT INTO shop (Shop_id, Belong, Name, Manager_id) VALUES (1, '美食园', '煎饼', 'admin1');
+INSERT INTO shop (Shop_id, Belong, Name, Manager_id) VALUES (2, '美食园', '麻辣香锅', 'admin2');
+INSERT INTO shop (Shop_id, Belong, Name, Manager_id) VALUES (3, '美食园', '湘里香外', 'admin3');
+INSERT INTO shop (Shop_id, Belong, Name, Manager_id) VALUES (4, '美食园', '强子烤冷面', 'admin4');
+INSERT INTO shop (Shop_id, Belong, Name, Manager_id) VALUES (5, '美食园', '沙县小吃', 'admin5');
 -- INSERT INTO Shop ('Shop_id', 'Belong', 'Name', 'Manager_id') VALUES ('', '', '', '');
 -- INSERT INTO Shop ('Shop_id', 'Belong', 'Name', 'Manager_id') VALUES ('', '', '', '');
 -- INSERT INTO Shop ('Shop_id', 'Belong', 'Name', 'Manager_id') VALUES ('', '', '', '');
